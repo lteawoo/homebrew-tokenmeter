bc444b3a01aa5937006e819280db71d004b53cb1
Taeu <lteawoo@naver.com>
2026-03-23 03:37:38 +0900

    Update tokenmeter cask to v0.1.2

--- Casks/tokenmeter.rb
+++ Casks/tokenmeter.rb
@@ -1,6 +1,6 @@
 cask "tokenmeter" do
-  version "0.1.0"
-  sha256 "80b33983a893883ce0bef970dd2909998f717259ce11741ff4e4633018978b48"
+  version "0.1.2"
+  sha256 "64f333a1d89081d039f5b7e4318080ac147c0e2361ee2c4449a56913d377d82d"
 
   url "https://github.com/lteawoo/TokenMeter/releases/download/v#{version}/TokenMeter_#{version}_aarch64.dmg"
   name "TokenMeter"
