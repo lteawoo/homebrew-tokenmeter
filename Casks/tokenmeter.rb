092efc6d4f266da367666cc4168b0af4056ccb43
lteawoo <lteawoo@naver.com>
2026-05-02 20:05:33 +0900

    chore: release tokenmeter 0.1.6

--- Casks/tokenmeter.rb
+++ Casks/tokenmeter.rb
@@ -1,6 +1,6 @@
 cask "tokenmeter" do
-  version "0.1.5"
-  sha256 "c063fe7c81bf803a2ec402c51826d0ec26e786ea54e66cb2e61a6833b6f4393d"
+  version "0.1.6"
+  sha256 "3f4201ae8e1658ab43bf28f1fcd17090ac77e73ff9271da66121c0bee5048e8c"
 
   url "https://github.com/lteawoo/TokenMeter/releases/download/v#{version}/TokenMeter_#{version}_aarch64.dmg"
   name "TokenMeter"
